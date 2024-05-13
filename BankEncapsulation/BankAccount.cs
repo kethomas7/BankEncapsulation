8217b33e6eda0bdec135394a69fd9ed90c73e438
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-05-13 10:48:49 -0500

    Completed Exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public string Type { get; set; } //Type of account Checkings or Savings
+        public string FirstName { get; set;}// First Name on the account
+
+        public string LastName { get; set; }//last name on the account
+
+
+    
+
+        public void Deposit (double moneyAdded){
+
+            Console.WriteLine("How much money do you want to add to the account?");
+            moneyAdded = Double.Parse(Console.ReadLine());
+         
+            balance += moneyAdded ;
+            Console.WriteLine($"Remaining Balance: {balance}");
+        }
+
+        public double GetBalance()
+        {
+            
+            return balance;
+        }
+        public void Withdraw (double moneyNeeded)
+        {
+            Console.WriteLine("How much money do you want to take out the account?");
+            moneyNeeded = Double.Parse(Console.ReadLine());
+
+            balance -= moneyNeeded;
+            Console.WriteLine($"Remaining Balance: {balance}");
+        }
+
+
+      
+
+        public static string CheckType (string type)
+        {
+            return type;
+        }
+
+        public void RecieptCheck()
+        {
+            Console.WriteLine("Would you like a reciept?");
+            if (Console.ReadLine().ToLower() == "yes")
+
+            {
+                Console.WriteLine($"Here is your reciept: \nSummary: Account Name: {FirstName} {LastName}\nAccount Type: {Type}\nBalance: {balance} ");
+            }
+            else
+            {
+                Console.WriteLine("Thank you for banking with Regions, Have a nice day.");
+            }
+
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount Account = new BankAccount();
+
+            Console.WriteLine("Welcome to Regions, What's the first name on the account?");
+            Account.FirstName = Console.ReadLine();
+
+
+            Console.WriteLine("What's the last name on the account?");
+            Account.LastName = Console.ReadLine();
+
+
+            Console.WriteLine($"Hello {Account.FirstName} {Account.LastName}, Is this a checkings or savings account?");
+            Account.Type = Console.ReadLine();
+
+            Console.WriteLine($"What would you like to do with this {Account.Type} account: Withdraw, Deposit, Check Balance");
+            string usersChoice = Console.ReadLine();
+
+
+            if (usersChoice == "Withdraw")
+            {
+
+                Account.Withdraw(Account.GetBalance());
+            }
+            else if (usersChoice == "Deposit")
+            {
+
+                Account.Deposit(Account.GetBalance());
+            }
+            else
+            {
+                Account.GetBalance();
+            }
+
+            Account.RecieptCheck();
         }
+
+
+
     }
 }
